69e42730166eaae5dad1eb686daa31c946958a0f
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-10-22 00:49:57 +0100

    fix: remove trailing slashes from url

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='spectacular-doc'),
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='spectacular-doc'),
-    path('strings/', ListCreateStringView.as_view(), name='list-create-strings'),
-    path('strings/filter-by-natural-language/', NaturalLanguageFilterView.as_view(), name='natural-language-filter-strings-search'),
-    path('strings/<str:string_value>/', RetrieveDeleteView.as_view(), name='retrieve-delete-string')
+    path('strings', ListCreateStringView.as_view(), name='list-create-strings'),
+    path('strings/filter-by-natural-language', NaturalLanguageFilterView.as_view(), name='natural-language-filter-strings-search'),
+    path('strings/<str:string_value>', RetrieveDeleteView.as_view(), name='retrieve-delete-string')
 ]
