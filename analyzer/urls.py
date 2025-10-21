f2674f9976c737b30a2e9c0df01d734181943ad3
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-10-21 06:39:42 +0100

    feat: add urls for the views created so far

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -1,22 +1,9 @@
-"""
-URL configuration for analyzer project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
 
+from base.views import ListCreateStringView, RetrieveDeleteView
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('strings/', ListCreateStringView.as_view(), name='list-create-strings'),
+    path('strings/<str:string_value>/', RetrieveDeleteView.as_view(), name='retrieve-delete-string')
 ]
