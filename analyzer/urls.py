3d5cf94e5c5917dea350816671983a16ecd3ff42
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-10-21 21:13:47 +0100

    update: add the url for natural language filter endpoint

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from base.views import ListCreateStringView, RetrieveDeleteView
+from base.views import ListCreateStringView, NaturalLanguageFilterView, RetrieveDeleteView
 
 
 urlpatterns = [
     path('strings/', ListCreateStringView.as_view(), name='list-create-strings'),
+    path('strings/filter-by-natural-language/', NaturalLanguageFilterView.as_view(), name='natural-language-filter-strings-search'),
     path('strings/<str:string_value>/', RetrieveDeleteView.as_view(), name='retrieve-delete-string')
 ]
